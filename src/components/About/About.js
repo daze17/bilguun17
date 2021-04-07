44c767cb9e1476135092391932a7cccaf5e22595
baasansuren <danzanshadav@gmail.com>
2021-04-07 18:49:07 +0800

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -30,9 +30,7 @@ function About( darkMode ) {
 
                             <div className="about-text-wrapper">
                                 <h1 className="about-title" data-aos="fade-up">ABOUT</h1>
-                                <p className="about-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                                    
-                                    
+                                <p className="about-text">Hello! I'm Bilguun Baasansuren,
                                     
                            
                                 </p>
